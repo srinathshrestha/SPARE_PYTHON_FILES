e9d24dd8bb04ca476aa1f5ca1a02bb93bffbff98
Srinath Shretha <srinathshrestha9890@gmail.com>
2021-01-02 12:56:45 +0530

    no'sum is the power to it's digit which then added up

---
+++ idk.py
@@ -0,0 +1,8 @@
+input = int(input('Enter the 4 digit number : '))
+
+sum_of_digit= int(input[0]) + int(input[1])+ int(input[2])+ int(input[3])
+def finall_result():
+    sum =  int(input[0])**sum_of_digit + int(input[1])**sum_of_digit +int(input[2])**sum_of_digit +int(input[3])**sum_of_digit
+    return sum
+
+(finall_result())
