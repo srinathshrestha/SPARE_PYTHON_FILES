8ae766129da2f86eb92dc56e51ee698405a1d3ce
Srinath Shretha <srinathshrestha9890@gmail.com>
2021-01-04 02:38:20 +0530

    no'sum is the power to it's digit which then added up

--- Armstrong.py
+++ Armstrong.py
@@ -2,11 +2,11 @@ a = int(input("Enter the number : \n" ))
 copy_a = a
 sum = 0
 order = len(str(a))
-while(0>a):
+while(a>0):
     d=a%10
     sum+= d ** order
     a=a//10
-if sum== copy_a:
+if (sum == copy_a):
     print(f'{copy_a} is a Armstrong number')
-else:
-    print(f'{copy_a} is not a Armstrong Number')
+# else:
+#     print(f'{copy_a} is not a Armstrong Number')
