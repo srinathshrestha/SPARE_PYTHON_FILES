d8adaf7e77131bff9efc2af4784da1ffa4765277
Srinath Shretha <srinathshrestha9890@gmail.com>
2021-01-04 14:27:03 +0530

    optimised

--- Factorial.py
+++ Factorial.py
@@ -5,4 +5,15 @@ def factorial(num):
         return num * factorial(num-1)
 
 
-print(factorial(int(input("Enter the number :\n"))))
+# print(factorial(int(input("Enter the number :\n"))))
+
+
+# -------------------------------------------------------------4
+
+def fact(n):
+    f = 1
+    for i in range(1,n+1):
+        f = f*i
+    return f
+x = fact(int(input("Enter the number :")))
+print(f'factorial is {x}')
--- Fibonacci.py
+++ Fibonacci.py
@@ -1,10 +1,40 @@
+# a = 0
+# b = 1
+# n = int(input("Enter how many element you want in the series "))
+# print(a,b, end=" ")
+# while n-2:
+#     c = a+b
+#     a=b
+#     b=c
+#     print(c,end=" ")
+#     n-=1
+
+n = int(input('Enter till how many no. you want : '))
 a = 0
 b = 1
-n = int(input("Enter how many element you want in the series "))
-print(a,b, end=" ")
-while n-2:
-    c = a+b
-    a=b
-    b=c
-    print(c,end=" ")
-    n-=1
+if n==1:
+    print(a)
+
+else:
+    print(a)
+    print(b)
+    for i in range(n):
+
+        print(a)
+        c = a
+        a = b
+        b = a+c
+
+
+        print(a+b)
+
+
+
+# def fib(n):
+#     prevnum = 0
+#     currentnum = 1
+#     for i in range(1,n):
+#         preprenum = prevnum
+#         prevnum = currentnum
+#         currentnum = preprenum+prevnum
+#     return currentnum
