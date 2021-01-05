e018738376ef26da2525a314627341dce65a3e71
Srinath Shretha <srinathshrestha9890@gmail.com>
2021-01-05 16:13:53 +0530

    dt : 05/01/2021

---
+++ PATTERNS.py
@@ -0,0 +1,17 @@
+# print('* '*4)
+# print('* '*4)
+# print('* '*4)
+# print('* '*4)
+n=int(input("Enter the number of the row you want : "))
+for i in range (n):
+        print('* '*n)
+
+
+for i in range(1,n+1):
+    print('* '*i)
+
+
+for i in range(n):
+    print(' '*(n-1)+'*'*i)
+
+
