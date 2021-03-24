c8701ff10ee90003fa694cddab729567c34d0a59
Srinath Shretha <srinathshrestha9890@gmail.com>
2021-03-24 18:11:14 +0530

    date : 24-03-2021

--- greater_smaller.py
+++ greater_smaller.py
@@ -1,6 +1,30 @@
-max  = 0
-for i in range(10):
-    a = int(input("Enter the no. : "))
-    if a>max:
-         max =a
-print(f"The maximum value you have entered is{max}")
+# prog to print the gratest amoung 10 inputed no.
+# max = 0
+# for i in range(10):
+#     a = int(input("Enter the no. : "))
+#     if a > max:
+#       max=a
+# print(f"The maximum value you have entered is {max}")
+
+# prog to print the smallest no amoung 10 :
+#
+# n = int(input("Enter the no. : "))
+# min=n
+# for i in range(9):
+#     n = int(input("Enter the no. : "))
+#     if n<min:
+#         min = n
+# print(f"The minimum value you have enterd is {min}.  ")
+
+
+# program to print weather the number os prime or not :
+# c=0
+# i = int(input("Enter the Number which you want to check for prime : "))
+# for k in  range(1,i+1):
+#     if i%k==0:
+#         c+=1
+# if c==2:
+#     print(f"The Number {i} is prime Number.")
+# else:
+#     print(f"The number {i} is not a prime number. ")
+
